7aa7b79f2cb105ade9ba022c92ea68b30adf5e57
Natalia Gordienko <nataliia.hordiienko@nure.ua>
2020-04-14 20:32:00 +0300

    TASK 0 - db connection

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const { json } = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const { connect } = require('mongoose');
+require('dotenv').config();
+
+const { BD_USER, BD_PASSWORD, BD_LINK } = process.env;
+connect(`mongodb+srv://${BD_USER}:${BD_PASSWORD}@${BD_LINK}`, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('DB connection complited successful');
+});
 
 const app = express();
 

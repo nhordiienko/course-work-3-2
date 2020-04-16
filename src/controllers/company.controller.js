e156e11f213af83cb92261fe71fe10cd6809ecc3
Natalia Gordienko <nataliia.hordiienko@nure.ua>
2020-04-16 22:33:35 +0300

    TASK 11 - delete company

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -2,18 +2,24 @@ const { Types } = require('mongoose');
 const Company = require('../schemas/company.schema');
 
 module.exports.addNew = async (req, res, next) => {
-  const { name } = req.body;
+  const {
+    name, address = '', occumpation = '', description = '',
+  } = req.body;
 
   const newCompany = new Company({
     _id: new Types.ObjectId(),
     name,
+    address,
+    occumpation,
+    description,
   });
 
   try {
     const result = await newCompany.save();
     res.status(201).json({
-      id: result._id,
-      name: result.name,
+      company: result,
+      getAllQuery: `${req.protocol}://${req.get('host')}/company`,
+      getQuery: `${req.protocol}://${req.get('host')}/company/${result._id}`,
     });
   } catch (error) {
     next(error);
@@ -46,8 +52,23 @@ module.exports.get = async (req, res, next) => {
   }
 };
 
-module.exports.delete = () => {
-
+module.exports.delete = async (req, res, next) => {
+  try {
+    const result = await Company.deleteOne({ _id: req.params.id });
+    if (result.n > 0) {
+      res.status(200).json({
+        message: 'Successfully deleted',
+        getAllQuery: `${req.protocol}://${req.get('host')}/company`,
+      });
+    } else {
+      res.status(304).json({
+        message: 'There are no companies with suck id',
+        getAllQuery: `${req.protocol}://${req.get('host')}/company`,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports.update = () => {
--- src/schemas/company.schema.js
+++ src/schemas/company.schema.js
@@ -3,6 +3,9 @@ const { Schema, model } = require('mongoose');
 const companySchema = Schema({
   _id: Schema.Types.ObjectId,
   name: { type: String, required: true, unique: true },
+  address: { type: String },
+  occumpation: { type: String },
+  description: { type: String },
 });
 
 module.exports = model('Company', companySchema);

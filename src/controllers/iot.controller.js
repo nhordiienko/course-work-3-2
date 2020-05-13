7a45ed55710e844f154aa399700194915448028d
Vladyslav Aleksieiev <vladyslav.aleksieiev@nure.ua>
2020-05-13 20:19:09 +0300

    buitify output iot

--- src/controllers/iot.controller.js
+++ src/controllers/iot.controller.js
@@ -6,5 +6,21 @@ module.exports.get = async (req, res) => {
   const respose = await fetch(IOT_URL);
   const data = await respose.json();
 
-  res.json({ data });
+  const { temperatureBuild } = data;
+  const tBuild = temperatureBuild.reduce((acc, cur) => acc + cur, 0) / temperatureBuild.length;
+  const { temperatureBody } = data;
+  const tBody = temperatureBody.reduce((acc, cur) => acc + cur, 0) / temperatureBody.length;
+  const { timeOfActivity } = data;
+  const time = [Math.min(...timeOfActivity), Math.max(...timeOfActivity)];
+  const { pressure } = data;
+  const p = pressure
+    .reduce(([A, B], [a, b]) => [A + a, B + b], [0, 0])
+    .map((val) => val / pressure.length);
+
+  res.json({
+    temperatureBuild: tBuild,
+    temperatureBody: tBody,
+    timeOfActivity: time,
+    pressure: p,
+  });
 }
